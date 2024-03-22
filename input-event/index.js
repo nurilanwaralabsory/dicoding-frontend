3dae7222b824410416f777a090a4282fec53d1a3
nurilanwaralabsory <nurilanwaralabsory@gmail.com>
2024-03-22 21:30:28 +0700

    event input pada form selesai

--- input-event/index.js
+++ input-event/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const inputMaxLengthOnLoad = document.getElementById("inputNama").maxLength;
   document.getElementById("sisaKarakter").innerText = inputMaxLengthOnLoad;
 
+  // menangani inputNama ketika batas maksimal hampir habis
   document.getElementById("inputNama").addEventListener("input", () => {
     const jumlahKarakterDiketik =
       document.getElementById("inputNama").value.length;
@@ -22,6 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+  // menangani inputNama ketika focus
   document.getElementById("inputNama").addEventListener("focus", function () {
     console.log("inputNama: focus");
     document.getElementById("notifikasiSisaKarakter").style.visibility =
@@ -31,5 +33,42 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("inputNama").addEventListener("blur", () => {
     document.getElementById("notifikasiSisaKarakter").style.visibility =
       "hidden";
+  });
+
+  document
+    .getElementById("inputCaptcha")
+    .addEventListener("change", function () {
+      console.log("inputCaptcha: change");
+      const inputCaptcha = document.getElementById("inputCaptcha").value;
+      const submitButtonStatus = document.getElementById("submitButton");
+      if (inputCaptcha === "PRNU") {
+        submitButtonStatus.removeAttribute("disabled");
+      } else {
+        submitButtonStatus.setAttribute("disabled", "");
+      }
+    });
+
+  // form ketika di submit dan menangani captcha
+  document
+    .getElementById("formDataDiri")
+    .addEventListener("submit", function (event) {
+      const inputCaptcha = document.getElementById("inputCaptcha").value;
+      if (inputCaptcha === "PRNU") {
+        alert("Selamat! Captcha Anda lolos :D");
+      } else {
+        alert("Captcha Anda belum tepat :(");
+        document.getElementById("submitButton").setAttribute("disabled", "");
+      }
+      event.preventDefault();
+    });
+
+  // event onCopy
+  document.getElementById("inputCopy").addEventListener("copy", function () {
+    alert("Anda telah men-copy sesuatu...");
+  });
+
+  // event onPaste
+  document.getElementById("inputPaste").addEventListener("paste", function () {
+    alert("Anda telah mem-paste sebuah teks...");
   });
 });
